e87b9ef810267253df9db21cbcda7aeeb16bc1e4
Michael Merickel <michael@merickel.org>
2012-03-13 02:15:10 -0500

    Updated classifier to support 3.2.

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.2",
         'Topic :: Database',
         'License :: OSI Approved :: MIT License',
     ],
